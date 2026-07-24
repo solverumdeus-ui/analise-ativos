c85fbab99e4b48e5f8681c6e9236e3145c1369f4
solverumdeus-ui <solverum.deus@gmail.com>
2026-07-24 06:39:32 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <nav>
               <Link href="/">ativos</Link>
               <Link href="/analises">episódios</Link>
+              <Link href="/metodo">método</Link>
               <Link href="/sobre">sobre</Link>
             </nav>
           </header>
